1ca22b8d2c2e27994b5242fd9bd253b9f5e9d0ee
ruhul <ra.01403288711@gmail.com>
2024-01-15 14:20:17 +0600

    ministry division id updated done

--- name_migration.py
+++ name_migration.py
@@ -35,9 +35,9 @@ def insert_source_name():
                 VALUES(0,0,0,0,%s)"""
                 cursor_dest.execute(insert_query_name,(new_source_name,))
                 mydb_connection_destinationdb.commit()
-                print(new_source_name)
+                print("Data inserted")
             else:
-                print(new_source_name,"-> already Exist")
+                print("Data already exist")
                 continue
 
     except Exception as E:
@@ -46,7 +46,12 @@ def insert_source_name():
         cursor_src.close()
         cursor_dest.close()
 
-def string_modification(position_index):
+def string_modification(name_string,index_position):
+    temp_data = name_string.split(',')
+    name = temp_data[index_position].replace(" ", "").lower()
+    return name
+
+def row_modification(position_index):
     cursor_dest = mydb_connection_destinationdb.cursor()
     modified_string_list = []
     try:
@@ -58,20 +63,49 @@ def string_modification(position_index):
                 temp_row = row[0].split(',')
                 ministry_name = temp_row[position_index]
                 modify_ministry_name = ministry_name.replace(" ", "").lower()
-                modified_string_list.append(modify_ministry_name)
+                modified_string_list.append([ministry_name,modify_ministry_name])
             except Exception as E:
                 continue
         return modified_string_list
     except Exception as E:
         print(str(E))
-def insert_ministry_id():
-    print(string_modification(3))
+
+def update_ministry_division_id():
+
+    try:
+        cursor_ministry_division_id = mydb_connection_destinationdb.cursor()
+
+    except Exception as E:
+        print(str(E))
+
+def update_ministry_id():
+    cursor_ministry = mydb_connection_destinationdb.cursor()
+    try:
+        query_get_ministry_info = """SELECT id,name FROM ministries"""
+        cursor_ministry.execute(query_get_ministry_info)
+        ministry_data  = cursor_ministry.fetchall()
+        for mindata in ministry_data:
+            id = mindata[0]
+            name = mindata[1]
+            query_getting_ids = """SELECT GROUP_CONCAT(id) AS id_list FROM ind_sources 
+            WHERE `name` LIKE  %s;"""
+            cursor_ministry.execute(query_getting_ids, (f"%{name}%",))
+            list_id = cursor_ministry.fetchall()[0][0]
+            query_update_ministry_id = """UPDATE ind_sources SET ministry_id = %s
+                        WHERE FIND_IN_SET(id,%s)"""
+            cursor_ministry.execute(query_update_ministry_id, ((id, list_id,)))
+            mydb_connection_destinationdb.commit()
+
+    except Exception as E:
+        print(str(E))
+
+
 
 
 
 
 if __name__ == "__main__":
-    #insert_source_name()
-    insert_ministry_id()
+    insert_source_name()
+    update_ministry_id()
     mydb_connection_sourcedb.close()
     mydb_connection_destinationdb.close()
