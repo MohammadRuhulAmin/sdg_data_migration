d8761f720a191f5b657ffadcc82b9fa75592c9e5
ruhul <ra.01403288711@gmail.com>
2024-01-15 11:06:21 +0600

    modified string function made

--- name_migration.py
+++ name_migration.py
@@ -16,7 +16,8 @@ mydb_connection_destinationdb = mysql.connector.connect(
     database="uat_sdg_tracker_clone"
 )
 
-def connection_data():
+
+def insert_source_name():
     cursor_src = mydb_connection_sourcedb.cursor()
     cursor_dest = mydb_connection_destinationdb.cursor()
     try:
@@ -36,6 +37,7 @@ def connection_data():
                 mydb_connection_destinationdb.commit()
                 print(new_source_name)
             else:
+                print(new_source_name,"-> already Exist")
                 continue
 
     except Exception as E:
@@ -44,10 +46,32 @@ def connection_data():
         cursor_src.close()
         cursor_dest.close()
 
+def string_modification(position_index):
+    cursor_dest = mydb_connection_destinationdb.cursor()
+    modified_string_list = []
+    try:
+        get_name_query = """SELECT name as process_name FROM uat_sdg_tracker_clone.ind_sources ;"""
+        cursor_dest.execute(get_name_query)
+        data = cursor_dest.fetchall()
+        for row in data:
+            try:
+                temp_row = row[0].split(',')
+                ministry_name = temp_row[position_index]
+                modify_ministry_name = ministry_name.replace(" ", "").lower()
+                modified_string_list.append(modify_ministry_name)
+            except Exception as E:
+                continue
+        return modified_string_list
+    except Exception as E:
+        print(str(E))
+def insert_ministry_id():
+    print(string_modification(3))
 
 
-# Call the function to fetch data
-connection_data()
 
-# Close the database connection when done
-mydb_connection_sourcedb.close()
+
+if __name__ == "__main__":
+    #insert_source_name()
+    insert_ministry_id()
+    mydb_connection_sourcedb.close()
+    mydb_connection_destinationdb.close()
---
+++ query.sql
@@ -0,0 +1,1 @@
+SELECT * FROM ministries WHERE LOWER(REPLACE(`name`, ' ', '')) = 'ministryofland';
