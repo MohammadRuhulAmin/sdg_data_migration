8b5bb8350b10acccff32e627ed48403900e5dd80
ruhul <ra.01403288711@gmail.com>
2024-02-01 13:09:03 +0600

    provider_id updated to indicator_data table

--- Project/indicator/additional_column/provider_id/controller/provider_id.py
+++ Project/indicator/additional_column/provider_id/controller/provider_id.py
@@ -14,7 +14,13 @@ def get_indicator_number_list_and_time_period__listfrom_uat():
                 cursor.execute(tpq.get_specific_user_info_by_time_period,(indicator_number,))
                 temp_data = cursor.fetchall()
                 for temp in temp_data:
-                    print(temp)
+                    data_period = temp[3]
+                    provider_id = temp[5]
+                    indicator_id = temp[7]
+                    cursor_uat.execute(tpq.update_provider_id,(provider_id,indicator_id,data_period,))
+                    mysql_connection.mydb_connection_destinationdb.commit()
+                    print(provider_id, " has been updated to " , indicator_id, data_period)
+
             except Exception as E:
                 continue
     except Exception as E:
--- Project/indicator/additional_column/provider_id/query/time_period.py
+++ Project/indicator/additional_column/provider_id/query/time_period.py
@@ -1,8 +1,8 @@
 get_specific_user_info_by_time_period = """
-    select temp1.serial_no,temp1.data_period,temp1.provider_id,temp2.username existing_user,
-    u.username uat_user,u.id uat_provider_id,
+    SELECT temp1.serial_no,temp1.provider_id,temp2.username existing_user,
+    temp1.data_period,u.username uat_user,u.id uat_provider_id,
     sid.indicator_number,sid.indicator_id
-    from(SELECT sil.serial_no,sidc.sdg_disaggregation_id,sidc.provider_id,stp.name data_period
+    FROM(SELECT sil.serial_no,sidc.sdg_disaggregation_id,sidc.provider_id,stp.name data_period
     #,spl.username
     FROM sdg_v1_v2_live.sdg_indicator_langs sil
     LEFT JOIN sdg_v1_v2_live.sdg_indicator_data sid ON sid.indicator_id = sil.indicator_id
@@ -10,17 +10,23 @@ get_specific_user_info_by_time_period = """
     LEFT JOIN sdg_v1_v2_live.sdg_time_periods stp ON stp.id = sid.time_period_id
     #LEFT JOIN sdg_provider_logs spl ON spl.sdg_provider_id = sidc.provider_id
     LEFT JOIN sdg_v1_v2_live.sdg_disaggregation_langs sdl ON sdl.disaggregation_id = sidc.sdg_disaggregation_id
-    WHERE sil.serial_no = "1.3.1" AND sil.language_id = 1 AND sid.status = 3 AND sid.publish IN(4,5)
-    and sidc.sdg_disaggregation_id = 1)temp1
-    left join(select distinct sdg_provider_id,username from sdg_v1_v2_live.sdg_provider_logs where status=3)temp2
-    on temp2.sdg_provider_id = temp1.provider_id
-    left join uat_sdg_tracker_clone.users u on u.username = temp2.username 
-    left join uat_sdg_tracker_clone.sdg_indicator_details sid 
-    on sid.indicator_number  COLLATE utf8_general_ci = temp1.serial_no  COLLATE utf8_general_ci and sid.language_id = 1;
+    WHERE sil.serial_no = %s AND sil.language_id = 1 AND sid.status = 3 AND sid.publish IN(4,5)
+    AND sidc.sdg_disaggregation_id = 1)temp1
+    LEFT JOIN(SELECT DISTINCT sdg_provider_id,username FROM sdg_v1_v2_live.sdg_provider_logs WHERE STATUS=3)temp2
+    ON temp2.sdg_provider_id = temp1.provider_id
+    LEFT JOIN uat_sdg_tracker_clone.users u ON u.username = temp2.username 
+    LEFT JOIN uat_sdg_tracker_clone.sdg_indicator_details sid 
+    ON sid.indicator_number  COLLATE utf8_general_ci = temp1.serial_no  COLLATE utf8_general_ci AND sid.language_id = 1;
 """
 
 indicator_wise_time_period_list = """
     select distinct sil.indicator_number from sdg_indicator_details sil
     where sil.language_id = 1 and sil.indicator_number  != ""
     order by sil.indicator_number;
+"""
+
+
+update_provider_id = """
+UPDATE indicator_data SET provider_id = %s
+WHERE ind_id = %s AND data_period = %s LIMIT 1;
 """
