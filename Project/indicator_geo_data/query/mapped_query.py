48878a55088c0302c5141d28ca56e05c5730dc16
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-15 15:49:08 +0600

    indicator_geo_data finally mapped

--- Project/indicator_geo_data/query/mapped_query.py
+++ Project/indicator_geo_data/query/mapped_query.py
@@ -5,3 +5,23 @@ WHERE sil.language_id = 1 AND sil.serial_no = "1.3.1")temp1
 LEFT JOIN sdg_indicator_data_children sidc ON sidc.indicator_data_id = temp1.indicator_data_id
 WHERE sidc.is_location = 1
 """
+
+
+mapped_query_2 = """
+SELECT tempx.serial_no,tempx.indicator_id,
+tempx.geo_division_id,gdiv.bbs_code,
+tempx.geo_district_id,gdis.bbs_code,
+tempx.geo_upazila_id,gupa.bbs_code
+FROM(SELECT temp1.serial_no,temp1.indicator_id,
+sidc.geo_division_id,
+sidc.geo_district_id,sidc.geo_upazila_id ,sidc.value 
+FROM (SELECT sil.serial_no,sid.indicator_id,sid.id indicator_data_id FROM sdg_indicator_langs sil
+LEFT JOIN sdg_indicator_data sid ON sid.indicator_id = sil.indicator_id
+WHERE sil.language_id = 1 AND sil.serial_no = "1.3.1")temp1
+LEFT JOIN sdg_indicator_data_children sidc ON sidc.indicator_data_id = temp1.indicator_data_id
+WHERE sidc.is_location = 1)tempx
+LEFT JOIN geo_divisions gdiv ON tempx.geo_division_id = gdiv.id
+LEFT JOIN geo_districts gdis ON tempx.geo_district_id = gdis.id
+LEFT JOIN geo_upazilas gupa  ON tempx.geo_upazila_id  = gupa.id
+
+"""
