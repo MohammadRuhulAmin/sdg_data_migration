b5555e61212fa4d2c494f3f08873810c433122c3
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-19 13:42:19 +0600

    new syntax added

--- Project/indicator/controller/indicator_geo_data.py
+++ Project/indicator/controller/indicator_geo_data.py
@@ -5,13 +5,14 @@ def indicator_geo_data(temp_json):
         print(temp_json)
         type = None
         ind_data_id = ind_data_values['ind_data_id']
-        geo_division_id = temp_json.get('geo_division_id')
-        geo_district_id = temp_json.get('geo_district_id')
-        geo_upazila_id  = temp_json.get('geo_upazila_id')
+        geo_division_id, geo_division_name, geo_district_id, geo_district_name, geo_upazila_id, geo_upazila_name = (
+            temp_json.get(key) for key in ['geo_division_id', 'geo_division_name', 'geo_district_id', 'geo_district_name', 'geo_upazila_id','geo_upazila_name']
+        )
+
         if geo_upazila_id !=0 and geo_district_id!=0 and geo_division_id!=0 :type   = 3
         if geo_upazila_id == 0 and geo_district_id !=0 and geo_division_id!=0:type  = 2
         if geo_upazila_id == 0 and geo_district_id == 0 and geo_division_id!=0:type = 1
-
+        print(type,geo_division_name,geo_district_name,geo_upazila_name)
 
     except Exception as E:
         print(str(E))
