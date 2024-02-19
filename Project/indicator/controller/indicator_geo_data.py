350193497175cf4adaa37c45fd5434949747bc40
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-19 14:15:47 +0600

    insert query on progress for indicator_geo_data table

--- Project/indicator/controller/indicator_geo_data.py
+++ Project/indicator/controller/indicator_geo_data.py
@@ -3,6 +3,7 @@ import Project.connection as mysql_connection
 import Project.indicator.query.geo_data_info as gdi
 def indicator_geo_data(temp_json):
     try:
+        cursor_dest = mysql_connection.mydb_connection_destinationdb.cursor()
         ind_data_id = ind_data_values['ind_data_id']
         type = None
         bbs_code = None
@@ -13,11 +14,23 @@ def indicator_geo_data(temp_json):
 
         if geo_upazila_id !=0 and geo_district_id!=0 and geo_division_id!=0 :
             type   = 3
-
+            cursor_dest.execute(gdi.upazila_bbs_code,(f"%{geo_upazila_name}%",))
+            row = cursor_dest.fetchall()
+            print("upaz")
+            print(geo_upazila_name)
+            print(row)
         if geo_upazila_id == 0 and geo_district_id !=0 and geo_division_id!=0:
             type  = 2
+            cursor_dest.execute(gdi.district_bbs_code, (f"%{geo_district_name}%",))
+            row = cursor_dest.fetchall()
+            print("dist")
+            print(row)
         if geo_upazila_id == 0 and geo_district_id == 0 and geo_division_id!=0:
             type = 1
+            cursor_dest.execute(gdi.district_bbs_code, (f"%{geo_division_name}%",))
+            row = cursor_dest.fetchall()
+            print("div")
+            print(row)
 
 
 
