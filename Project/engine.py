fc2201e4f082afeeb5925d0333eeb6d85aec1f67
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-28 15:26:41 +0600

    steps and engine function are done

--- Project/engine.py
+++ Project/engine.py
@@ -1,27 +1,32 @@
 
 import Project.truncate_data.truncate_tables as tt
-import Project.indicator.controller.indicator_main_tempx as imt
 import Project.sources.controller.source_main as sm
 import Project.users.controller.user_insert_main as uim
 import Project.users.controller.user_role as ur
 import Project.users.controller.user_types as ut
+import Project.create_alter_table.create_table.indicator_data.indicator_data_create as idc
+import Project.indicator.controller.indicator_main_tempx as imt
 
 def engine():
     try:
-
         # step1: alter all the necessary tables
-
         # step2: truncate all the tables
         tt.truncate_rapper()
-
-        # step3: map and insert ind_sources table
+        # step3 : create indicator_data table
+        idc.create_indicator_data()
+        # step4: map and insert ind_sources table
         sm.source_rapper()
-
-        # step4: users, user_role, user_type table migrate
+        # step5: users, user_role, user_type table migrate
         uim.get_indicator()
         ur.get_all_user_role()
         ut.new_user_type()
-
+        # step6: indicator entry
         imt.indicator_rapper_main()
+
+
     except Exception as E:
         print(str(E))
+
+
+if __name__ == "__main__":
+    engine()
