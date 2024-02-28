bf94ffe65183190052feece336572dcfd6ff9a71
Md. Ruhul Amin <ruhulamin.cs.dev@gmail.com>
2024-02-28 17:00:32 +0600

    user_types alter operation for before and after done

--- Project/create_alter_table/alter_operation/after_migration/users.py
+++ Project/create_alter_table/alter_operation/after_migration/users.py
@@ -31,9 +31,28 @@ def alter_user_role():
         print(str(E))
 
 
-
+def alter_user_type():
+    try:
+        alter_user_type_ddl = """
+        alter table user_types Modify id varchar(255),
+        Modify type_name varchar(255) not NULL,
+        Modify is_registrable int(11) not NULL,
+        Modify security_profile_id int(11) not NULL,
+        Modify auth_token_type enum('optional','mandatory') not NULL,
+        Modify is_default tinyint(1) not NULL,
+        Modify status enum('active','inactive') not NULL,
+        Modify created_by int(11) not NULL,
+        Modify updated_by int(11) not NULL
+        """
+        cursor_dest = mysql_connection.mydb_connection_destinationdb.cursor()
+        cursor_dest.execute(alter_user_type_ddl)
+        mysql_connection.mydb_connection_destinationdb.commit()
+        print("user_type table has been altered for data migration")
+    except Exception as E:
+        print(str(E))
 
 
 if __name__ == "__main__":
     alter_users()
     alter_user_role()
+    alter_user_type()
--- Project/create_alter_table/alter_operation/before_migration/users.py
+++ Project/create_alter_table/alter_operation/before_migration/users.py
@@ -29,6 +29,27 @@ def alter_user_role():
         cursor_dest = mysql_connection.mydb_connection_destinationdb.cursor()
         cursor_dest.execute(alter_user_role_ddl)
         mysql_connection.mydb_connection_destinationdb.commit()
+        print("user_role table has been altered for data migration")
+    except Exception as E:
+        print(str(E))
+
+def alter_user_type():
+    try:
+        alter_user_type_ddl = """
+        alter table user_types Modify id int(10) auto_increment,
+        Modify type_name varchar(255)  NULL,
+        Modify is_registrable int(11) NULL,
+        Modify security_profile_id int(11) NULL,
+        Modify auth_token_type enum('optional','mandatory') NULL,
+        Modify is_default tinyint(1) NULL,
+        Modify status enum('active','inactive') NULL,
+        Modify created_by int(11) NULL,
+        Modify updated_by int(11) NULL
+        """
+        cursor_dest = mysql_connection.mydb_connection_destinationdb.cursor()
+        cursor_dest.execute(alter_user_type_ddl)
+        mysql_connection.mydb_connection_destinationdb.commit()
+        print("user_type table has been altered for data migration")
     except Exception as E:
         print(str(E))
 
@@ -36,3 +57,4 @@ def alter_user_role():
 if __name__ == "__main__":
     alter_users()
     alter_user_role()
+    alter_user_type()
